c35b15cd891f97bbdab91443d6fbf6dfe833ddc0
Camilla Matos <camillapozerm@gmail.com>
2020-10-04 20:09:15 -0300

    refactor: move Httphelper to a folder

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,6 +1,6 @@
 import { Authentication, HttpRequest, Validation } from './login-protocols'
 import { MissingParamError } from '../../errors'
-import { badRequest, serverError, unauthorized, ok } from '../../helpers/http-helper'
+import { badRequest, serverError, unauthorized, ok } from '../../helpers/http/http-helper'
 import { LoginController } from './login'
 
 interface SutTypes {
