726c08d179f2fb988783877ca98e4295734e42f5
Camilla Matos <camillapozerm@gmail.com>
2020-10-05 21:14:25 -0300

    test: ensure DbAuthentication return a token on success

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -137,4 +137,11 @@ describe('DbAuthentication', () => {
 
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return a token on success', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth(makeFakeAuthentication())
+
+    expect(accessToken).toBe('any_token')
+  })
 })
