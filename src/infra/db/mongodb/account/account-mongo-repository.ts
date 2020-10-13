4460cbb68ae2133fd4311991cd42370dc3444f2e
Camilla Matos <camillapozerm@gmail.com>
2020-10-13 09:57:51 -0300

    feat: ensure SurveyMongoRepository add a survey on success

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -8,7 +8,6 @@ import { MongoHelper } from '../helpers/mongo-helper'
 export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
-
     const result = await accountCollection.insertOne(accountData)
 
     return MongoHelper.map(result.ops[0])
