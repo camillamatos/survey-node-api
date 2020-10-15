1c8cc7d1d3ed0edaebd1914cfee5daa11bd84efa
Camilla Matos <camillapozerm@gmail.com>
2020-10-15 16:35:24 -0300

    test: ensure JwtAdapter return a value os verify success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -49,5 +49,12 @@ describe('Jwt Adapter', () => {
       await sut.decrypt('any_token')
       expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
+
+    test('Should return a value os verify success', async () => {
+      const sut = makeSut()
+
+      const value = await sut.decrypt('any_token')
+      expect(value).toBe('any_value')
+    })
   })
 })
