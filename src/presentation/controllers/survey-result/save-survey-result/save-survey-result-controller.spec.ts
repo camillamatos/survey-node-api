d05a7466f38658b1be3d257f501f046df4f55f0e
Camilla Matos <camilla@hygiabank.com>
2020-12-30 18:26:20 -0300

    test: ensure SaveSurveyResultController returns 500 if SaveSurveyResult throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -134,4 +134,14 @@ describe('SaveSurveyResult Controller', () => {
       answer: 'any_answer'
     })
   })
+
+  test('Should return 500 if SaveSurveyResult throws', async () => {
+    const { sut, saveSurveyResultStub } = makeSut()
+
+    jest.spyOn(saveSurveyResultStub, 'save').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+
+    const httResponse = await sut.handle(makeFakeRequest())
+
+    expect(httResponse).toEqual(serverError(new Error()))
+  })
 })
